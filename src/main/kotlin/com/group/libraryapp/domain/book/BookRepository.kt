49172c9f65d9791c0444721cd1ed2117ce8a01c9
hj <parkhj062@gmail.com>
2025-12-04 23:46:41 +0900

    xxRepository.java -> xxRepository.kt

---
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -0,0 +1,8 @@
+package com.group.libraryapp.domain.book
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface BookRepository : JpaRepository<Book, Long> {
+    fun findByName(name: String): Optional<Book>
+}
