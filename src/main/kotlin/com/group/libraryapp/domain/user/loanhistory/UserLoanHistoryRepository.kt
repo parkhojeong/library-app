183908252e209f5d69aac2024c47b87591bbbfc1
hj <parkhj062@gmail.com>
2025-12-06 20:56:25 +0900

    UserLoanHistoryRepository.kt > findByBookName

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -3,9 +3,9 @@ package com.group.libraryapp.domain.user.loanhistory
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserLoanHistoryRepository: JpaRepository<UserLoanHistory, Long> {
-    fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
+    fun findByBookName(bookName: String): UserLoanHistory?
 
-    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
+    fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
 
     fun countByStatus(status: UserLoanStatus): Long
 }
